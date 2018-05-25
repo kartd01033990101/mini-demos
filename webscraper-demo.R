ac3e7457c9c8ba382938d328ff3542aaaba2c970
Sam Lee <kartd01033990101@qq.com>
2018-05-24 19:20:29 -0700

    Attempt demo exercises

--- webscraper-demo.R
+++ webscraper-demo.R
@@ -26,19 +26,19 @@ head(rank_data)
 rank_data<-as.numeric(rank_data)
 head(rank_data)
 
-#Using CSS selectors to scrape the title section
-
+title_data_html <- html_nodes(webpage, ".lister-item-header a")
 #html to text 
-
+title_data <- html_text(title_data_html)
 #look at data
-
+head(title_data)
 #Using CSS selectors to scrape the description section
-
+description_data_html <- html_nodes(webpage,'.ratings-bar+ .text-muted')
 #Converting the description data to text
-
+description_data <- html_text(description_data_html)
 #look at data
-
+head(description_data)
 #Data-Preprocessing: removing '\n'
+description_data<-gsub("\n","",description_data)
 
 #Using CSS selectors to scrap the Movie runtime section
 
